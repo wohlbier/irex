c26cf911e786a2a1fb303564a8e1c42727716270
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-06-25 11:37:59 -0400

    clean up spacing.

--- main.cc
+++ main.cc
@@ -3,9 +3,9 @@
 
 int main (int argc, char *argv[])
 {
-    size_t N=1024;
+    size_t N = 1024;
     int a = 0;
-    for (size_t i=0; i<N; i++)
+    for (size_t i = 0; i < N; i++)
     {
         a += rand()%2;
     }
